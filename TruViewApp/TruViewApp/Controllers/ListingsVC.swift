c3a3c490d9c6d2482100a128197da643e08c1451
NormanLiana <liananorman@pursuit.org>
2020-02-13 14:57:17 -0500

    adds constraints to the 4 objects, adds them as subviews of the view, updates VC background color

--- TruViewApp/TruViewApp/Controllers/ListingsVC.swift
+++ TruViewApp/TruViewApp/Controllers/ListingsVC.swift
@@ -46,8 +46,56 @@ class ListingsVC: UIViewController {
     var halfOpenSlideCardViewTopConstraint: NSLayoutConstraint?
     var fullScreenSlideCardTopConstraint: NSLayoutConstraint?
     
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        addSubViews()
+        addConstraints()
+        setUpInitialVCViews()
+    }
+    
+    // MARK: - Private Methods
+    private func setUpInitialVCViews() {
+        view.backgroundColor = .white
+    }
+    
+    private func addSubViews() {
+        view.addSubview(searchBar)
+        view.addSubview(filterMenuButton)
+        view.addSubview(mapListViewSegController)
+        view.addSubview(mapView)
+    }
+    
+    private func addConstraints() {
+        constrainSearchBar()
+        constrainFilterMenuButton()
+        constrainSegmentedController()
+        constrainMapView()
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainSearchBar() {
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        [searchBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor), searchBar.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.825), searchBar.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.1)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainFilterMenuButton() {
+        filterMenuButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        [filterMenuButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor), filterMenuButton.leadingAnchor.constraint(equalTo: searchBar.trailingAnchor), filterMenuButton.trailingAnchor.constraint(equalTo: view.trailingAnchor), filterMenuButton.heightAnchor.constraint(equalTo: searchBar.heightAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainSegmentedController() {
+        mapListViewSegController.translatesAutoresizingMaskIntoConstraints = false
+        
+        [mapListViewSegController.topAnchor.constraint(equalTo: searchBar.bottomAnchor), mapListViewSegController.leadingAnchor.constraint(equalTo: view.leadingAnchor), mapListViewSegController.trailingAnchor.constraint(equalTo: view.trailingAnchor), mapListViewSegController.heightAnchor.constraint(equalTo: searchBar.heightAnchor, multiplier: 0.4)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainMapView() {
+        mapView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [mapView.topAnchor.constraint(equalTo: mapListViewSegController.bottomAnchor), mapView.leadingAnchor.constraint(equalTo: view.leadingAnchor), mapView.trailingAnchor.constraint(equalTo: view.trailingAnchor), mapView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)].forEach({$0.isActive = true})
     }
 
 
