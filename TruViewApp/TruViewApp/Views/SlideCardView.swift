7eeffc48a11ddabfda61f38b7d43f2e34fb4559e
NormanLiana <liananorman@pursuit.org>
2020-02-13 15:20:54 -0500

    adds 6 UI objects, 2 image views, 3 labels and 1 text view

--- TruViewApp/TruViewApp/Views/SlideCardView.swift
+++ TruViewApp/TruViewApp/Views/SlideCardView.swift
@@ -10,12 +10,44 @@ import UIKit
 
 class SlideCardView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
+    // MARK: - UI Objects
+    lazy var upArrowIndicator: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(systemName: "arrowtriangle.up")
+        img.tintColor = .black
+        return img
+    }()
+    
+    lazy var aptThumbnail: UIImageView = {
+        let img = UIImageView()
+        img.image = UIImage(systemName: "bed.double")
+        img.backgroundColor = .blue
+        return img
+    }()
+    
+    lazy var priceLabel: UILabel = {
+        let label = UILabel()
+        label.text = "$2000"
+        label.textAlignment = .right
+        return label
+    }()
+    
+    lazy var bedAndBathLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Beds: 2 Baths: 1"
+        return label
+    }()
+    
+    lazy var sqFootageLabel: UILabel = {
+        let label = UILabel()
+        label.text = "700 Sq. Feet"
+        return label
+    }()
+    
+    lazy var aptDescriptionTextView: UITextView = {
+        let txtView = UITextView()
+        txtView.text = "Decription goes here"
+        return txtView
+    }()
 
 }
