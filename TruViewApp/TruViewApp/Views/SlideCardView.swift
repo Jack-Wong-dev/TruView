106d65f7924dd4f9fe2f8e7af953efd1666c9c54
NormanLiana <liananorman@pursuit.org>
2020-02-13 15:24:22 -0500

    adds constraints to all objects and adds them as subviews of the view

--- TruViewApp/TruViewApp/Views/SlideCardView.swift
+++ TruViewApp/TruViewApp/Views/SlideCardView.swift
@@ -49,5 +49,78 @@ class SlideCardView: UIView {
         txtView.text = "Decription goes here"
         return txtView
     }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        addConstraints()
+        setUpViewUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func addSubViews() {
+        addSubview(upArrowIndicator)
+        addSubview(aptThumbnail)
+        addSubview(priceLabel)
+        addSubview(bedAndBathLabel)
+        addSubview(sqFootageLabel)
+        addSubview(aptDescriptionTextView)
+    }
+    
+    private func addConstraints() {
+        constrainArrowImage()
+        constrainAptThumbnailImage()
+        constrainBedAndBathlabel()
+        constrainSqFootagelabel()
+        constrainPricelabel()
+        constrainAptTextView()
+    }
+    
+    private func setUpViewUI() {
+        backgroundColor = .gray
+        layer.cornerRadius = 20
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainArrowImage() {
+        upArrowIndicator.translatesAutoresizingMaskIntoConstraints = false
+        
+        [upArrowIndicator.topAnchor.constraint(equalTo: topAnchor), upArrowIndicator.centerXAnchor.constraint(equalTo: centerXAnchor), upArrowIndicator.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.031), upArrowIndicator.widthAnchor.constraint(equalTo: upArrowIndicator.heightAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainAptThumbnailImage() {
+        aptThumbnail.translatesAutoresizingMaskIntoConstraints = false
+        
+        [aptThumbnail.topAnchor.constraint(equalTo: upArrowIndicator.bottomAnchor, constant: 10), aptThumbnail.leadingAnchor.constraint(equalTo: leadingAnchor), aptThumbnail.trailingAnchor.constraint(equalTo: trailingAnchor), aptThumbnail.heightAnchor.constraint(equalTo: widthAnchor, multiplier: 0.7)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainBedAndBathlabel() {
+        bedAndBathLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [bedAndBathLabel.topAnchor.constraint(equalTo: aptThumbnail.bottomAnchor), bedAndBathLabel.leadingAnchor.constraint(equalTo: leadingAnchor), bedAndBathLabel.trailingAnchor.constraint(equalTo: centerXAnchor), bedAndBathLabel.heightAnchor.constraint(equalTo: aptThumbnail.heightAnchor, multiplier: 0.2)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainSqFootagelabel() {
+        sqFootageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [sqFootageLabel.topAnchor.constraint(equalTo: bedAndBathLabel.bottomAnchor), sqFootageLabel.leadingAnchor.constraint(equalTo: leadingAnchor), sqFootageLabel.trailingAnchor.constraint(equalTo: centerXAnchor), sqFootageLabel.heightAnchor.constraint(equalTo: bedAndBathLabel.heightAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainPricelabel() {
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [priceLabel.topAnchor.constraint(equalTo: aptThumbnail.bottomAnchor), priceLabel.leadingAnchor.constraint(equalTo: centerXAnchor), priceLabel.trailingAnchor.constraint(equalTo: trailingAnchor), priceLabel.bottomAnchor.constraint(equalTo: bedAndBathLabel.bottomAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainAptTextView() {
+        aptDescriptionTextView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [aptDescriptionTextView.topAnchor.constraint(equalTo: centerYAnchor), aptDescriptionTextView.centerXAnchor.constraint(equalTo: centerXAnchor), aptDescriptionTextView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.85), aptDescriptionTextView.bottomAnchor.constraint(equalTo: bottomAnchor)].forEach({$0.isActive = true})
+    }
 
 }
