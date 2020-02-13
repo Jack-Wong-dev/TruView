a3cc163c4a3d4beb526fd0ff95504c1c58da2ae3
NormanLiana <liananorman@pursuit.org>
2020-02-13 14:41:48 -0500

    adds 4 UI objects, search bar, filter button, segmented controller and map view

--- TruViewApp/TruViewApp/Controllers/ListingsVC.swift
+++ TruViewApp/TruViewApp/Controllers/ListingsVC.swift
@@ -7,24 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
 class ListingsVC: UIViewController {
 
+    // MARK: - UI Objects
+    lazy var searchBar: UISearchBar = {
+        let sb = UISearchBar()
+        sb.searchBarStyle = .minimal
+        return sb
+    }()
+    
+    lazy var filterMenuButton: UIButton = {
+        let btn = UIButton()
+        btn.setTitle("Filter", for: .normal)
+        btn.setTitleColor(.blue, for: .normal)
+        return btn
+    }()
+    
+    lazy var mapListViewSegController: UISegmentedControl = {
+        let items = ["Map View", "List View"]
+        let sc = UISegmentedControl(items: items)
+        sc.selectedSegmentIndex = 0
+//        sc.addTarget(self, action: #selector(segControlValueChanged(_:)), for: .valueChanged)
+        return sc
+    }()
+    
+    lazy var mapView: MKMapView = {
+        let mv = MKMapView()
+        return mv
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
