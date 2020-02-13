b582ca1c00504e7915468a91df69bac65a5a49d5
NormanLiana <liananorman@pursuit.org>
2020-02-13 15:16:10 -0500

    adds constraints to all 4 objects and adds them as a subview of the content view

--- TruViewApp/TruViewApp/Views/ListingCVCell.swift
+++ TruViewApp/TruViewApp/Views/ListingCVCell.swift
@@ -34,4 +34,55 @@ class ListingCVCell: UICollectionViewCell {
             lbl.text = "600 sq Feet"
             return lbl
         }()
+    
+    // MARK: - Initializers
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubViews()
+        addConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func addSubViews() {
+        addSubview(aptThumbnail)
+        addSubview(priceLabel)
+        addSubview(bedAndbathLabel)
+        addSubview(sqFootageLabel)
+    }
+    
+    private func addConstraints() {
+        constrainThumbnailImage()
+        constrainBedAndBathLabel()
+        constrainSqFootageLabel()
+        constrainPriceLabel()
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainThumbnailImage() {
+        aptThumbnail.translatesAutoresizingMaskIntoConstraints = false
+        
+        [aptThumbnail.topAnchor.constraint(equalTo: contentView.topAnchor), aptThumbnail.leadingAnchor.constraint(equalTo: contentView.leadingAnchor), aptThumbnail.trailingAnchor.constraint(equalTo: contentView.trailingAnchor), aptThumbnail.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.7)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainBedAndBathLabel() {
+        bedAndbathLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [bedAndbathLabel.topAnchor.constraint(equalTo: aptThumbnail.bottomAnchor), bedAndbathLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor), bedAndbathLabel.trailingAnchor.constraint(equalTo: contentView.centerXAnchor), bedAndbathLabel.heightAnchor.constraint(equalTo: aptThumbnail.heightAnchor, multiplier: 0.2)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainSqFootageLabel() {
+        sqFootageLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [sqFootageLabel.topAnchor.constraint(equalTo: bedAndbathLabel.bottomAnchor), sqFootageLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor), sqFootageLabel.trailingAnchor.constraint(equalTo: contentView.centerXAnchor), sqFootageLabel.heightAnchor.constraint(equalTo: bedAndbathLabel.heightAnchor)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainPriceLabel() {
+        priceLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        [priceLabel.topAnchor.constraint(equalTo: aptThumbnail.bottomAnchor), priceLabel.leadingAnchor.constraint(equalTo: contentView.centerXAnchor), priceLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor), priceLabel.heightAnchor.constraint(equalTo: bedAndbathLabel.heightAnchor)].forEach({$0.isActive = true})
+    }
 }
