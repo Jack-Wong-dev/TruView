a261f1805e7304e2bdd2533199aaf7a357469397
NormanLiana <liananorman@pursuit.org>
2020-02-20 11:55:34 -0500

    creates UI object for custom view, adds constraints and adds as a subview of the VC

--- TruViewApp/TruViewApp/Controllers/CreateListingVC.swift
+++ TruViewApp/TruViewApp/Controllers/CreateListingVC.swift
@@ -9,22 +9,43 @@
 import UIKit
 
 class CreateListingVC: UIViewController {
+    
+    // MARK: - UI Objects
+    let createListingView: CreateListingView = {
+        let view = CreateListingView()
+        return view
+    }()
 
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubViews()
+        addConstraints()
+        setUpVCViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(createListingView)
     }
-    */
+    
+    private func addConstraints() {
+        constrainCreateListingView()
+    }
+    
+    private func setUpVCViews() {
+        view.backgroundColor = .white
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainCreateListingView() {
+        createListingView.translatesAutoresizingMaskIntoConstraints = false
+        
+        [createListingView.topAnchor.constraint(equalTo: view.topAnchor), createListingView.leadingAnchor.constraint(equalTo: view.leadingAnchor), createListingView.trailingAnchor.constraint(equalTo: view.trailingAnchor), createListingView.bottomAnchor.constraint(equalTo: view.bottomAnchor)].forEach({$0.isActive = true})
+    }
+    
+    
+    
 
+    
 }
