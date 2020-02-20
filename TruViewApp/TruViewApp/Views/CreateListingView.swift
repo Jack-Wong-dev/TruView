0d47d6a4e3ebff89d5bdb395eabf07cee0ac5aeb
NormanLiana <liananorman@pursuit.org>
2020-02-20 11:53:40 -0500

    adds constraints to purchase type seg controller and adds as a subview of the custom view

--- TruViewApp/TruViewApp/Views/CreateListingView.swift
+++ TruViewApp/TruViewApp/Views/CreateListingView.swift
@@ -21,10 +21,33 @@ class CreateListingView: UIView {
     // MARK: - Initializers
     override init(frame: CGRect) {
         super.init(frame: frame)
+        addSubViews()
+        addConstraints()
+        setUpViewUI()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private Methods
+    private func addSubViews() {
+        addSubview(purchaseTypeSegController)
+    }
+    
+    private func addConstraints() {
+        constrainPurchaseTypeSegControl()
+    }
+    
+    private func setUpViewUI() {
+        backgroundColor = .white
+    }
+    
+    // MARK: - Constraint Methods
+    private func constrainPurchaseTypeSegControl() {
+        purchaseTypeSegController.translatesAutoresizingMaskIntoConstraints = false
+        
+        [purchaseTypeSegController.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor), purchaseTypeSegController.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.92), purchaseTypeSegController.centerXAnchor.constraint(equalTo: centerXAnchor), purchaseTypeSegController.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.04)].forEach({$0.isActive = true})
     }
     
 }
