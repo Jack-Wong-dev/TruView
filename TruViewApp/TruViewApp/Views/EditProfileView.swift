34c4770458061b1b48412cb21f2fc0676528a703
Ian Cervone <ian@Ians-MacBook-Pro.local>
2020-02-21 15:28:52 -0500

    added EditProfileView file to Views folder

---
+++ TruViewApp/TruViewApp/Views/EditProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileView.swift
+//  TruViewApp
+//
+//  Created by Ian Cervone on 2/21/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
