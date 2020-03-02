2867be6779d3ee8391dad01c639627eab5447a00
NormanLiana <liananorman@pursuit.org>
2020-03-02 10:26:40 -0500

    adds cocoa file for starting point picker view

---
+++ TruViewApp/TruViewApp/Views/StartingPointPickerView.swift
@@ -0,0 +1,21 @@
+//
+//  StartingPointPickerView.swift
+//  TruViewApp
+//
+//  Created by Liana Norman on 3/2/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class StartingPointPickerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
