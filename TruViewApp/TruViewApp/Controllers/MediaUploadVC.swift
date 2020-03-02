a8eba6e9552c57daa3b559a008c87e35219cebfe
NormanLiana <liananorman@pursuit.org>
2020-03-01 22:53:41 -0500

    adds method for library permissions and set image picker up in delegate WIP

--- TruViewApp/TruViewApp/Controllers/MediaUploadVC.swift
+++ TruViewApp/TruViewApp/Controllers/MediaUploadVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Photos
 
 class MediaUploadVC: UIViewController {
 
@@ -16,11 +17,20 @@ class MediaUploadVC: UIViewController {
         return view
     }()
     
+    // MARK: - Properties
+    var photoLibraryAccessIsAuthorized = false
+    var imagesForPanoCV = AllRoomData.imageCollection {
+        didSet{
+            mediaUploadView.panoImageCV.reloadData()
+        }
+    }
+    
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         addSubViews()
         setUpVCView()
+        checkPhotoLibraryAccess()
     }
     
     // MARK: - Private Methods
@@ -32,5 +42,48 @@ class MediaUploadVC: UIViewController {
         view.backgroundColor = .white
     }
     
+    private func checkPhotoLibraryAccess() {
+        let status = PHPhotoLibrary.authorizationStatus()
+    
+        switch status {
+        case .authorized:
+            print(status)
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization { [weak self] (status) in
+                switch status {
+                case .authorized:
+                    self?.photoLibraryAccessIsAuthorized = true
+                case .notDetermined:
+                    print("not determined")
+                case .restricted:
+                    print("restricted")
+                case .denied:
+                    self?.photoLibraryAccessIsAuthorized = false
+                @unknown default:
+                    fatalError("This is outside of any authorization case.")
+                }
+            }
+        case .restricted:
+            print("restricted")
+        case .denied:
+            photoLibraryAccessIsAuthorized = false
+        @unknown default:
+            print("nothing should happen here")
+        }
+    }
 
+}
+
+// MARK: - Extensions
+extension CreateListingVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let imagePreviewVC = ImagePreviewVC()
+        if let image = info[.originalImage] as? UIImage {
+            imagePreviewVC.currentImage = image
+        }
+//        imagePreviewVC.delegate = self
+        dismiss(animated: true) {
+            self.present(imagePreviewVC, animated: true, completion: nil)
+        }
+    }
 }
