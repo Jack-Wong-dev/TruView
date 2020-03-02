8473ab69e7696d149c5f6f70aa3bc2e27e79eb99
NormanLiana <liananorman@pursuit.org>
2020-03-01 21:21:53 -0500

    adds files for media upload view and VC

---
+++ TruViewApp/TruViewApp/Views/MediaUploadView.swift
@@ -0,0 +1,21 @@
+//
+//  MediaUploadView.swift
+//  TruViewApp
+//
+//  Created by Liana Norman on 3/1/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class MediaUploadView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
