a5b80905eece703a7d8e71faf5672e9bc95fbf63
NormanLiana <liananorman@pursuit.org>
2020-03-02 15:15:09 -0500

    creates UI objects for cancel and save buttons and adds as a subview with constraints

--- TruViewApp/TruViewApp/Views/MediaUploadView.swift
+++ TruViewApp/TruViewApp/Views/MediaUploadView.swift
@@ -11,6 +11,22 @@ import UIKit
 class MediaUploadView: UIView {
 
     // MARK: - UI Objects
+    lazy var cancelButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Cancel", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .white
+        return button
+    }()
+    
+    lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Save", for: .normal)
+        button.setTitleColor(.blue, for: .normal)
+        button.backgroundColor = .white
+        return button
+    }()
+    
     lazy var thumbnailImageCV: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .horizontal
@@ -44,20 +60,36 @@ class MediaUploadView: UIView {
     
     // MARK: - Private Methods
     private func addSubViews() {
+        addSubview(cancelButton)
+        addSubview(saveButton)
         addSubview(thumbnailImageCV)
         addSubview(panoImageCV)
     }
     
     private func addConstraints() {
+        constrainCancelButton()
+        constrainSaveButton()
         constrainThumbnailCV()
         constrainPanoCV()
     }
     
     // MARK: - Constriant Methods
+    private func constrainCancelButton() {
+        cancelButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        [cancelButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor), cancelButton.leadingAnchor.constraint(equalTo: leadingAnchor), cancelButton.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.25), cancelButton.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.05)].forEach({$0.isActive = true})
+    }
+    
+    private func constrainSaveButton() {
+        saveButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        [saveButton.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor), saveButton.trailingAnchor.constraint(equalTo: trailingAnchor), saveButton.widthAnchor.constraint(equalTo: cancelButton.widthAnchor), saveButton.heightAnchor.constraint(equalTo: cancelButton.heightAnchor)].forEach({$0.isActive = true})
+    }
+    
     private func constrainThumbnailCV() {
         thumbnailImageCV.translatesAutoresizingMaskIntoConstraints = false
         
-        [thumbnailImageCV.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor), thumbnailImageCV.leadingAnchor.constraint(equalTo: leadingAnchor), thumbnailImageCV.trailingAnchor.constraint(equalTo: trailingAnchor), thumbnailImageCV.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.25)].forEach({$0.isActive = true})
+        [thumbnailImageCV.topAnchor.constraint(equalTo: cancelButton.bottomAnchor), thumbnailImageCV.leadingAnchor.constraint(equalTo: leadingAnchor), thumbnailImageCV.trailingAnchor.constraint(equalTo: trailingAnchor), thumbnailImageCV.heightAnchor.constraint(equalTo: safeAreaLayoutGuide.heightAnchor, multiplier: 0.25)].forEach({$0.isActive = true})
     }
     
     private func constrainPanoCV() {
