2b5f8f613e968f26f570abdb2c921c572a1b1a1e
NormanLiana <liananorman@pursuit.org>
2020-03-01 22:20:54 -0500

    creates UI object for custom media upload view and adds as a subview, sets VCs background color to white

--- TruViewApp/TruViewApp/Controllers/MediaUploadVC.swift
+++ TruViewApp/TruViewApp/Controllers/MediaUploadVC.swift
@@ -10,21 +10,27 @@ import UIKit
 
 class MediaUploadVC: UIViewController {
 
+    // MARK: - UI Objects
+    lazy var mediaUploadView: MediaUploadView = {
+        let view = MediaUploadView()
+        return view
+    }()
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubViews()
+        setUpVCView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(mediaUploadView)
     }
-    */
+    
+    private func setUpVCView() {
+        view.backgroundColor = .white
+    }
+    
 
 }
