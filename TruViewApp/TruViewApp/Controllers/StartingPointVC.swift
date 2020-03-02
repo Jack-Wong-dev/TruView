b1c77f98e49cc3b4138df9ea3e085ab9f8426ff7
NormanLiana <liananorman@pursuit.org>
2020-03-02 15:08:57 -0500

    creates object for starting point picker view and adds as a subview, sets views background to white

--- TruViewApp/TruViewApp/Controllers/StartingPointVC.swift
+++ TruViewApp/TruViewApp/Controllers/StartingPointVC.swift
@@ -10,21 +10,28 @@ import UIKit
 
 class StartingPointVC: UIViewController {
 
+    // MARK: - UI Objects
+    lazy var startPointView: StartingPointPickerView = {
+        let view = StartingPointPickerView()
+        return view
+    }()
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubViews()
+        setUpVCView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(startPointView)
     }
-    */
+    
+    private func setUpVCView() {
+        view.backgroundColor = .white
+    }
+    
+    
 
 }
