a328d1ab532d5a27ddc2c631b0866dfe0566db91
NormanLiana <liananorman@pursuit.org>
2020-03-02 19:09:52 -0500

    refactors name of button from create tour to upload photos

--- TruViewApp/TruViewApp/Views/CreateListingView.swift
+++ TruViewApp/TruViewApp/Views/CreateListingView.swift
@@ -145,7 +145,7 @@ class CreateListingView: UIView {
         return tv
     }()
     
-    lazy var createTourButton: UIButton = {
+    lazy var uploadPhotosButton: UIButton = {
         let button = UIButton()
         button.setTitle("Create Tour", for: .normal)
         button.backgroundColor = .orange
@@ -187,7 +187,7 @@ class CreateListingView: UIView {
         addSubview(priceTextField)
         addSubview(descriptionLabel)
         addSubview(descriptionTextView)
-        addSubview(createTourButton)
+        addSubview(uploadPhotosButton)
         
     }
     
@@ -348,9 +348,9 @@ class CreateListingView: UIView {
     }
     
     private func constrainCreateTourButton() {
-        createTourButton.translatesAutoresizingMaskIntoConstraints = false
+        uploadPhotosButton.translatesAutoresizingMaskIntoConstraints = false
         
-        [createTourButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: frame.height * 0.05), createTourButton.widthAnchor.constraint(equalTo: streetAddressLabel.widthAnchor), createTourButton.centerXAnchor.constraint(equalTo: centerXAnchor), createTourButton.heightAnchor.constraint(equalTo: streetAddressLabel.heightAnchor, multiplier: 1.25)].forEach({$0.isActive = true})
+        [uploadPhotosButton.topAnchor.constraint(equalTo: descriptionTextView.bottomAnchor, constant: frame.height * 0.05), uploadPhotosButton.widthAnchor.constraint(equalTo: streetAddressLabel.widthAnchor), uploadPhotosButton.centerXAnchor.constraint(equalTo: centerXAnchor), uploadPhotosButton.heightAnchor.constraint(equalTo: streetAddressLabel.heightAnchor, multiplier: 1.25)].forEach({$0.isActive = true})
     }
     
 }
