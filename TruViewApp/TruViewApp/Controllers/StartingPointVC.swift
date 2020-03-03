e77efffd381153915ec23536da9bfa024c199c75
NormanLiana <liananorman@pursuit.org>
2020-03-03 14:42:48 -0500

    adds dismiss functionality to cancel button and adds segue to tour editor VC functionality to next button

--- TruViewApp/TruViewApp/Controllers/StartingPointVC.swift
+++ TruViewApp/TruViewApp/Controllers/StartingPointVC.swift
@@ -13,6 +13,8 @@ class StartingPointVC: UIViewController {
     // MARK: - UI Objects
     lazy var startPointView: StartingPointPickerView = {
         let view = StartingPointPickerView()
+        view.cancelButton.addTarget(self, action: #selector(cancelButtonPressed), for: .touchUpInside)
+        view.nextButton.addTarget(self, action: #selector(nextButtonPressed), for: .touchUpInside)
         return view
     }()
     
@@ -21,6 +23,19 @@ class StartingPointVC: UIViewController {
         super.viewDidLoad()
         addSubViews()
         setUpVCView()
+    }
+    
+    // MARK: - Actions
+    @objc func cancelButtonPressed() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @objc func nextButtonPressed() {
+        let graph = GraphData.manager.populateGraph()
+        let startingRoom = graph.getRoom(name: graph.firstRoomID!)!
+        let tourEditorVC = TourEditorVC(graph: graph, room: startingRoom)
+        tourEditorVC.modalPresentationStyle = .fullScreen
+        present(tourEditorVC, animated: true, completion: nil)
     }
     
     // MARK: - Private Methods
