f620518e93862d6b3f548d752e0203ff4ab96a9a
NormanLiana <liananorman@pursuit.org>
2020-03-08 17:10:05 -0400

    adds 2 files for cover and tour photo manager views

---
+++ TruViewApp/TruViewApp/Views/CoverPhotoManagerView.swift
@@ -0,0 +1,21 @@
+//
+//  CoverPhotoManagerView.swift
+//  TruViewApp
+//
+//  Created by Liana Norman on 3/8/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class CoverPhotoManagerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ TruViewApp/TruViewApp/Views/TourPhotoManagerView.swift
@@ -0,0 +1,21 @@
+//
+//  TourPhotoManagerView.swift
+//  TruViewApp
+//
+//  Created by Liana Norman on 3/8/20.
+//  Copyright © 2020 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class TourPhotoManagerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
