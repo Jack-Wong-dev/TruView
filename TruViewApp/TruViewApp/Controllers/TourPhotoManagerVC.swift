cc5dd8affab03a055626a84179d9704dd0536731
NormanLiana <liananorman@pursuit.org>
2020-03-09 11:57:50 -0400

    creates UI object with custom tour photo mngr view and adds as a subview, sets background color of VC

--- TruViewApp/TruViewApp/Controllers/TourPhotoManagerVC.swift
+++ TruViewApp/TruViewApp/Controllers/TourPhotoManagerVC.swift
@@ -10,21 +10,29 @@ import UIKit
 
 class TourPhotoManagerVC: UIViewController {
 
+    // MARK: - UI Objects
+    lazy var tourPhtMngrView: TourPhotoManagerView = {
+        let view = TourPhotoManagerView()
+        return view
+    }()
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        addSubViews()
+        setUpVCView()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Private Methods
+    private func addSubViews() {
+        view.addSubview(tourPhtMngrView)
     }
-    */
+    
+    private func setUpVCView() {
+        view.backgroundColor = .systemBackground
+    }
 
 }
