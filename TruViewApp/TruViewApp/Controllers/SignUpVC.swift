783cec4e44530a9d947f19963ffb525ad12764ab
Ian Cervone <ian@Ians-MacBook-Pro.local>
2020-03-10 13:52:05 -0400

    added views, and began to set up firebase to create a new user, code to create user is still in progress and will not currently work

--- TruViewApp/TruViewApp/Controllers/SignUpVC.swift
+++ TruViewApp/TruViewApp/Controllers/SignUpVC.swift
@@ -7,24 +7,97 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class SignUpVC: UIViewController {
+  
+  var emailEntered: String? = nil {
+    didSet {
+      
+    }
+  }
+  
+  var passwordEntered: String? = nil {
+    didSet {
+      
+    }
+  }
+  
+  
+  // MARK: UI Objects
+
+        lazy var signUpViews: SignUpView = {
+          let su = SignUpView()
+          su.signUpButton.addTarget(self, action: #selector(signUpButtonPressed), for: .touchUpInside)
+          su.loginButton.addTarget(self, action: #selector(logInButtonPressed), for: .touchUpInside)
+          return su
+        }()
+              
+        
+  //MARK: Lifecycle
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+      super.viewDidLoad()
+      view.addSubview(signUpViews)
+      setDelgates()
+    }
+  
+    func setDelgates() {
+        signUpViews.emailTextField.delegate = self
+        signUpViews.passwordTextField.delegate = self
     }
     
 
-    /*
-    // MARK: - Navigation
+  
+  //MARK: Obj-C methods
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func signUpButtonPressed() {
+        guard let email = emailEntered, let password = passwordEntered else {
+          showAlert(title: "Error", message: "Please enter an email and password.")
+            return
+        }
+
+        guard Utilities.isEmailValid(email) else {
+          showAlert(title: "Error", message: "Please enter a valid email.")
+            return
+        }
+
+      guard Utilities.isPasswordValid(password) else {
+        showAlert(title: "Error", message: "Password must have at least 8 characters.")
+            return
+        }
+
+  //      FirebaseAuthService.manager.createNewUser(email: email.lowercased(), password: password) { [weak self] (result) in
+  //          self?.handleCreateAccountResponse(with: result)
+  //      }
+      
+      let nextVC = EditProfileVC()
+      print("login button pressed")
+      nextVC.modalPresentationStyle = .overFullScreen
+      present(nextVC, animated: true, completion: nil)
     }
-    */
 
+    @objc func logInButtonPressed() {
+           let loginVC = LogInVC()
+           print("button pressed")
+           loginVC.modalPresentationStyle = .formSheet
+           present(loginVC, animated: true, completion: nil)
+       }
+    
+  
+  
+
+}
+
+extension SignUpVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+      if textField == signUpViews.emailTextField {
+            emailEntered = textField.text
+            textField.resignFirstResponder()
+      } else if textField == signUpViews.passwordTextField {
+            passwordEntered = textField.text
+            textField.resignFirstResponder()
+      }
+      return true
+    }
 }
