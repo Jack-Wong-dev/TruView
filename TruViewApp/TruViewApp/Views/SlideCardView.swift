cdbf5067ee2a1a73e9c94b0767b9d8e6399e6125
NormanLiana <liananorman@pursuit.org>
2020-03-16 20:53:48 -0400

    updates button's title label font

--- TruViewApp/TruViewApp/Views/SlideCardView.swift
+++ TruViewApp/TruViewApp/Views/SlideCardView.swift
@@ -55,6 +55,7 @@ class SlideCardView: UIView {
         let button = UIButton()
         button.setTitle("Contact Agent", for: .normal)
         button.backgroundColor = #colorLiteral(red: 0.4256733358, green: 0.5473166108, blue: 0.3936028183, alpha: 1)
+        button.titleLabel?.font = UIFont(name: "BanglaSangamMN-Bold", size: 18)
         button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
         button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
         button.layer.shadowOpacity = 1.0
