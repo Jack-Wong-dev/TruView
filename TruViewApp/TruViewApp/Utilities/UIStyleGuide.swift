e44fea24e20363d23c1176d219b2d6b4f17e439f
NormanLiana <liananorman@pursuit.org>
2020-03-25 16:06:51 -0400

    adds style guide to highlight bottom line of TF in red if no text is present

--- TruViewApp/TruViewApp/Utilities/UIStyleGuide.swift
+++ TruViewApp/TruViewApp/Utilities/UIStyleGuide.swift
@@ -42,6 +42,28 @@ class Utilities {
         
     }
     
+    static func styleHighlightedTextField(_ textfield:UITextField) {
+        
+        // Create the bottom line
+        let bottomLine = CALayer()
+        
+        bottomLine.frame = CGRect(x: 0, y: textfield.frame.height - 2, width: textfield.frame.width, height: 1)
+        
+        //        bottomLine.frame = CGRect(x: 0, y: 50 - 2, width: 300, height: 2)
+        
+        bottomLine.backgroundColor = UIColor.red.cgColor
+        
+        // Remove border on text field
+        textfield.borderStyle = .none
+        
+        // Add the line to the text field
+        textfield.layer.addSublayer(bottomLine)
+        
+        
+        textfield.returnKeyType = .next
+        
+    }
+    
     static func styleTextView(_ textView: UITextView) {
         
         let bottomLine = CALayer()
