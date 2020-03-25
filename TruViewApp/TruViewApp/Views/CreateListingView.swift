54b15d6357a921771bb209247fd63cb081ccc61d
NormanLiana <liananorman@pursuit.org>
2020-03-25 18:52:22 -0400

    adds shadows and rounds corners of save and cancel buttons

--- TruViewApp/TruViewApp/Views/CreateListingView.swift
+++ TruViewApp/TruViewApp/Views/CreateListingView.swift
@@ -17,6 +17,12 @@ class CreateListingView: UIView {
         button.setTitleColor(#colorLiteral(red: 0.4256733358, green: 0.5473166108, blue: 0.3936028183, alpha: 1), for: .normal)
         button.backgroundColor = .systemBackground
         button.titleLabel?.font = UIFont(name: "BanglaSangamMN-Bold", size: 21)
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowRadius = 2.0
+        button.layer.masksToBounds = false
+        button.layer.cornerRadius = 4.0
         return button
     }()
     
@@ -26,6 +32,12 @@ class CreateListingView: UIView {
         button.setTitleColor(#colorLiteral(red: 0.4256733358, green: 0.5473166108, blue: 0.3936028183, alpha: 1), for: .normal)
         button.backgroundColor = .systemBackground
         button.titleLabel?.font = UIFont(name: "BanglaSangamMN-Bold", size: 21)
+        button.layer.shadowColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.25).cgColor
+        button.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+        button.layer.shadowOpacity = 1.0
+        button.layer.shadowRadius = 2.0
+        button.layer.masksToBounds = false
+        button.layer.cornerRadius = 4.0
         return button
     }()
     
